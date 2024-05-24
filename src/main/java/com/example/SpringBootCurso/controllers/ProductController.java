cac7cb9c9e94deef90b1fa04aca53bc97b0a8272
Abc Bolinhas <abc@bolinhas.com>
2024-05-24 16:47:22 -0300

    arrumando primeiro bug

--- src/main/java/com/example/SpringBootCurso/controllers/ProductController.java
+++ src/main/java/com/example/SpringBootCurso/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.example.SpringBootCurso.repositories.ProductRepository;
 import jakarta.validation.Valid;;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/products")
 public class ProductController {
 
